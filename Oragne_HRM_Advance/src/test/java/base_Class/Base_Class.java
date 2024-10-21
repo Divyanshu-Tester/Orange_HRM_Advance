b958121f00385c7d23a2aa49ae3107308be7d6e9
Divyanhsu Priyadarshi <divyanshupriyadarshi@gmail.com>
2024-10-21 11:31:33 +0530

    Pom.xml Update

--- Oragne_HRM_Advance/src/test/java/base_Class/Base_Class.java
+++ Oragne_HRM_Advance/src/test/java/base_Class/Base_Class.java
@@ -1,5 +1,24 @@
 package base_Class;
 
-public class Base_Class {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Base_Class 
+
+{
+  public WebDriver driver ;
+  
+  @BeforeMethod
+  public void Launch_Chrome()
+  {
+	  WebDriverManager.chromedriver().setup();
+	  driver=new ChromeDriver();
+	  driver.manage().window().maximize();
+	  driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	  
+  }
+  
 }
